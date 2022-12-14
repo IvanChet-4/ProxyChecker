1d934b553982dcc15a1c554d36d84bf80cbe286c
IvanChet-4 <wokewylf-111@mail.ru>
2022-12-14 09:52:26 +0300

    Исправлено оформление

--- src/Main.java
+++ src/Main.java
@@ -5,21 +5,18 @@ import static constants.Constants.PATH_TO_FILE_for_save_goods_ip;
 import static constants.Constants.PATH_TO_FILE_somes_ip;
 
 public class Main {
-
     public static void main(String[] args) {
-
         try {
             FileInputStream fis = new FileInputStream(PATH_TO_FILE_somes_ip); //  Путь к файлу ".../Desktop/ip.txt", в котором содержаться ip-адреса и port-порты для прокси соединения.
             String address = "";
             int i;
-
+            /* Читаем файл содержащий номера портов и ip */
             while ((i = fis.read()) != -1) {
-
                 if (i == 13) continue;
                 else if (i == 10) {
                     String ip = address.split("\t")[0];
                     int port = Integer.parseInt(address.split("\t")[1]);
-
+                    /* Запускаем проверку и запись успешного результата */
                     Thread thread = new Thread(new Runnable() {
                         @Override
                         public void run() {
@@ -28,7 +25,6 @@ public class Main {
                         }
                     });
                     thread.start();
-
                     address = "";
                 } else {
                     address += (char) i;
@@ -39,22 +35,22 @@ public class Main {
         }
     }
 
-
+    /* Проверяем подключение к url через прокси и в случае успешного подключеня записываем результат (ip, port) в файл goods_ip.txt */
     public static void checkProxy(String ip, int port) {
-
         try {
+            /* Подключаем и проверяем */
             Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress(ip, port));
             URL url = new URL("https://vozhzhaev.ru/test.php");
             // URL url = new URL("https://google.com");
             // URL url = new URL("https://www.youtube.com/");
             URLConnection connection = url.openConnection(proxy);
-
             BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
             String inputline = "";
             while ((inputline = in.readLine()) != null) {
                 System.out.println(inputline + " - работает -> ====GOODS-IP====");
             }
 
+            /* Записываем результат */
             FileWriter fw = new FileWriter(PATH_TO_FILE_for_save_goods_ip, true); //  Путь к файлу ".../Desktop/goods_ip.txt", в котором сохраняются ip-адреса и номера портов по которым устанавливается успешное соединение.
             fw.write("\n " + ip + " : " + port + " \n \r");
             fw.flush();
